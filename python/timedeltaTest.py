18a1f9169948d3a27c54cfce808e14e7536e5635
kbhat1234 <karthikbhat@live.com>
2017-09-26 00:50:05 +0530

    Adding timedeltaTest.py

---
+++ python/timedeltaTest.py
@@ -0,0 +1,25 @@
+from datetime import date, time, datetime, timedelta
+
+def timedelta_func():
+           timeDelta = timedelta(days=365, hours=8, minutes=15)
+           print str(timeDelta)
+           
+           print "Today is: ", str(datetime.now())
+           print "Today is: ", str(date.today())
+
+           print "date 1 year from now: ", str(datetime.now() + timedelta(days=365))
+           print "date 2 months from now: ", str(datetime.now() + timedelta(days=60))
+
+           print "in one week and 4 days it will be ", str(datetime.now() + timedelta(weeks=1, days=4))
+           today = date.today()
+           nyd = date(2018,1,1)
+           if(nyd < today):
+                      print "New year day already went by " +str((today-nyd).days) + " ago"
+           else:
+                      print "New year yet to come by "
+
+def main():
+           timedelta_func()
+
+if __name__ == "__main__":
+           main()
