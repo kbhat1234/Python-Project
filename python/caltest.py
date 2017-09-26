1ca974c720f0f4704cb8629af499a1af02a923d7
kbhat1234 <karthikbhat@live.com>
2017-09-26 20:07:05 +0530

    change caltest.py

--- python/caltest.py
+++ python/caltest.py
@@ -1,8 +1,8 @@
 import calendar
 
 cal = calendar.month(2017, 9)
-print cal
-print calendar.prcal(2017)
+#print cal
+#print calendar.prcal(2017)
 print calendar.firstweekday()
 print calendar.isleap(2015)
 print calendar.monthcalendar(2017,10)
