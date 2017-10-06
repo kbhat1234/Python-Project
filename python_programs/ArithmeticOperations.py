203ae99c8a1b0206d09d126c23c68a92b62b4f62
kbhat1234 <karthikbhat@live.com>
2017-10-06 20:15:53 +0530

    adding py files

---
+++ python_programs/ArithmeticOperations.py
@@ -0,0 +1,34 @@
+def addition():
+           print "Addition operation"
+           num1=float(input("Enter first number:"))
+           num2=float(input("Enter second number:"))
+           sum=num1+num2
+           print "Addition of {0} and {1} is {2} ".format(num1, num2, sum)
+
+def substraction():
+           print "Substraction operation"
+           num1=float(input("Enter first number:"))
+           num2=float(input("Enter second number:"))
+           min=num1-num2
+           print "Min of {0} and {1} is {2}".format(num1, num2, min)
+
+def multiply():
+           print "Multiplication operation"
+           num1=float(input("Enter first number:"))
+           num2=float(input("Enter second number:"))
+           mul=num1*num2
+           print "Mul of {0} and {1} is {2}".format(num1, num2, mul)
+
+def division():
+           print "Division operation"
+           num1=float(input("Enter first number:"))
+           num2=float(input("Enter second number:"))
+           div=num1/num2
+           print "Div of {0} and {1} is {2}".format(num1, num2, div)
+
+def modulus():
+           print "Modulus operation"
+           num1=float(input("Enter first number:"))
+           num2=float(input("Enter second number:"))
+           mod=num1%num2
+           print "Mod of {0} and {1} is {2}".format(num1, num2, mod)
---
+++ python_programs/ArithmeticTest.py
@@ -0,0 +1,11 @@
+from ArithmeticOperations import addition, substraction, multiply, division, modulus
+
+def main():
+           addition()
+           substraction()
+           multiply()
+           division()
+           modulus()
+
+if __name__ == "__main__":
+           main()
