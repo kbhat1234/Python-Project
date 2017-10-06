3027789bd16e5c09ff15f8325b07fddef6151921
kbhat1234 <karthikbhat@live.com>
2017-10-06 20:57:11 +0530

    Adding py files

---
+++ python_programs/Traingle.py
@@ -0,0 +1,13 @@
+def AreaTraingle():
+           l=float(input("Enter length:"))
+           b=float(input("enter breadth:"))
+           area=0.5*l*b;
+           print "Area of traingle is ", area
+
+"""def SemiPerimeter():
+           s1=float(input("Enter 1st side:"))
+           s2=float(input("Enter 2nd side:"))
+           s3=float(input("Enter 3rd side:"))
+           perimeter=(s1+s2+s3)/2
+           print "Perimeter of traingle:", perimeter"""
+           
---
+++ python_programs/TraingleTest.py
@@ -0,0 +1,8 @@
+from Traingle import AreaTraingle
+
+def main():
+           AreaTraingle()
+           """SemiPerimeter()"""
+
+if __name__ == "__main__":
+           main()
